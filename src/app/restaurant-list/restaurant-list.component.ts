1bc4738ec350c5be355d16624c8c079493aadf31
Tripti Rajput <tripti.rajput05@gmail.com>
2021-06-18 15:39:13 +0530

    post-food screen

--- src/app/restaurant-list/restaurant-list.component.ts
+++ src/app/restaurant-list/restaurant-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  restaurants : any;
 
   ngOnInit(): void {
+    let response = this.http.get("https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/18e3b6a2b3b13f76c05cc25d0d54f92505ea3971689337fa3293f378db3d298c/food_posting/entries");
+    response.subscribe((data: any)=>{
+      console.log(data.entries);
+      this.restaurants=data.entries;
+    });
   }
-
 }
